606bad9aeda2b8a892cd54dbcebe72239c67025b
fameal <facu@fameal.com.ar>
2020-11-23 20:14:43 -0300

    Added zGovernance stats

--- src/ZGovernance.vue
+++ src/ZGovernance.vue
@@ -8,28 +8,27 @@
         div(class="gov-panel")
           div(class="gov-title") <strong>ZGOVERNANCE STATS</strong>
           div(class="gov-body")
-            div Total Staked: 8,888.00 <span class="unit">ZLOT</span>
-            div Reward Rate: 88,800 <span class="unit">ZLOT/WEEK</span>
+            div Total Staked: {{ total_staked | fromWei(2) }} <span class="unit">ZLOT</span>
     div(class="row")
       div(class="column")
         div(class="gov-panel")
           div(class="gov-title") <strong>ZTREASURY STATS</strong>
           div(class="gov-body")
-            div <span class="label">Earnings Distributed:</span> 29,823.1888 <span class="unit">ZHEGIC</span>
-            div <span class="label">Last Distribution:</span> Thursday at 4:03 pm
-            div <span class="label">Maintainer's Share:</span> 10 %
-            div <span class="label">Governance Share:</span> 90%
+            div <span class="label">Earnings Distributed:</span> {{ total_earnings_distributed | fromWei(6) }} <span class="unit">ZHEGIC</span>
+            div <span class="label">Last Distribution:</span> {{ last_earnings_distribution | fromTimestamp }}
+            div <span class="label">Maintainer's Share:</span> {{ maintainer_share/shares_precision/100 | toPct(0) }}
+            div <span class="label">Governance Share:</span> {{ governance_share/shares_precision/100 | toPct(0) }}
     div(class="row")
       div(class="column")
         div(class="gov-panel")
           div(class="gov-title") <strong>YOUR STATS</strong>
           div(class="gov-body")
-            div <span class="label">Staked:</span> 88 <span class="unit">ZLOT</span>
-            div <span class="label">% of Staked ZLOT:</span> 88.88%
-            div <span class="label">ZLOT Balance:</span> 8,888.88  <span class="unit">ZLOT</span>
-            div <span class="label">Total Reward Claimed:</span> 88 <span class="unit">ZHEGIC</span>
-            div <span class="label">Reward Rate:</span> 888.88 <span class="unit">ZHEGIC/WEEK</span>
-            div <span class="label">Rewards Available:</span> 888 <span class="unit">ZHEGIC</span>
+            div <span class="label">Staked:</span> {{ your_stake | fromWei(6) }} <span class="unit">ZLOT</span>
+            div <span class="label">% of Staked ZLOT:</span> {{ your_stake/total_staked | toPct(2) }}
+            div <span class="label">ZLOT Balance:</span> {{ zlot_balance | fromWei(2) }}  <span class="unit">ZLOT</span>
+            div <span class="label">Total Reward Claimed:</span> {{ total_earned_rewards | fromWei(2) }} <span class="unit">ZHEGIC</span>
+            div <span class="label">Reward Rate:</span> {{ reward_rate | fromWei(2) }} <span class="unit">ZHEGIC/WEEK</span>
+            div <span class="label">Rewards Available:</span> {{ earned | fromWei(2) }} <span class="unit">ZHEGIC</span>
 
 
 </template>
@@ -37,8 +36,8 @@
 <script>
 //import InfoPanel from './components/InfoPanel'
 import { mapGetters } from 'vuex'
-//import ethers from 'ethers'
-//import axios from 'axios'
+import ethers from 'ethers'
+import moment from 'moment'
 
 //const max_uint = new ethers.BigNumber.from(2).pow(256).sub(1).toString()
 
@@ -55,12 +54,85 @@ export default {
       error: null
     }
   },
-  filters: {},
-  methods: {},
+  filters: {
+    fromWei(data, precision) {
+      if (data === 'loading') return data
+      if (data > 2**255) return '�~H~^'
+      let value = ethers.utils.commify(ethers.utils.formatEther(data))
+      let parts = value.split('.')
+
+      if (precision === 0) return parts[0]
+
+      return parts[0] + '.' + parts[1].slice(0, precision)
+    },
+    toPct(data, precision) {
+      if (isNaN(data)) return '?'
+      return `${(data * 100).toFixed(precision)}%`
+    },
+    fromTimestamp(data) {
+      if (data) {
+        return moment.unix(String(data)).calendar()
+      }
+    },
+  },
+  methods: {
+    call(contract, method, args, out='number') {
+      let key = this.drizzleInstance.contracts[contract].methods[method].cacheCall(...args)
+      let value
+      try {
+        value = this.contractInstances[contract][method][key].value 
+      } catch (error) {
+        value = null
+      }
+      switch (out) {
+        case 'number':
+          if (value === null) value = 0
+          return new ethers.BigNumber.from(value)
+        case 'address':
+          return value
+        default:
+          return value
+      }
+    },
+  },
   computed: {
     ...mapGetters('accounts', ['activeAccount', 'activeBalance']),
     ...mapGetters('drizzle', ['drizzleInstance', 'isDrizzleInitialized']),
     ...mapGetters('contracts', ['getContractData', 'contractInstances']),
+    zlot_balance() {
+      return this.call('zLOT', 'balanceOf', [this.activeAccount])
+    },
+    total_earnings_distributed() {
+      return this.call('zTreasury', 'totalEarningsDistributed', [])
+    },
+    last_earnings_distribution() {
+      return this.call('zTreasury', 'lastEarningsDistribution', [])
+    },
+    maintainer_share(){
+      return this.call('zTreasury', 'maintainerShare', [])
+    },
+    governance_share(){
+      return this.call('zTreasury', 'governanceShare', [])
+    },
+    shares_precision(){
+      return this.call('zTreasury', 'SHARES_PRECISION', [])
+    },
+    your_stake() {
+      return this.call('zGovernance', 'balanceOf', [this.activeAccount])
+    },
+    total_staked() {
+      return this.call('zGovernance', 'totalSupply', [])
+    },
+    earned() {
+      return this.call('zGovernance', 'earned', [this.activeAccount])
+    },
+    reward_rate() {
+      return this.call('zGovernance', 'rewardRate', [])
+    },
+    total_earned_rewards() {
+      return this.call('zGovernance', 'totalEarnedRewards', [this.activeAccount])
+    },
+
   },  
 }
 
--- src/drizzleOptions.js
+++ src/drizzleOptions.js
@@ -2,6 +2,9 @@ import ERC20 from './abi/ERC20.json'
 import HegicStaking from './abi/HegicStaking.json'
 import LotManager from './abi/LotManager.json'
 import HegicPool from './abi/HegicPool.json'
+import zTreasury from './abi/zTreasury.json'
+import zGovernance from './abi/zGovernance.json'
+import UNIV2 from './abi/UNIV2.json'
 
 import Web3 from 'web3'
 let web3 = new Web3(Web3.givenProvider);
@@ -14,6 +17,14 @@ const options = {
       web3Contract: new web3.eth.Contract(ERC20, "0x584bc13c7d411c00c01a62e8019472de68768430")
     },
     {
+      contractName: 'zHEGIC',
+      web3Contract: new web3.eth.Contract(ERC20, "0x837010619aeb2ae24141605afc8f66577f6fb2e7")
+    },
+    {
+      contractName: 'zLOT',
+      web3Contract: new web3.eth.Contract(ERC20, "0xa8e7ad77c60ee6f30bac54e2e7c0617bd7b5a03e")
+    },
+    {
       contractName: 'HegicStakingETH',
       web3Contract: new web3.eth.Contract(HegicStaking, "0x1Ef61E3E5676eC182EED6F052F8920fD49C7f69a")
     },
@@ -22,16 +33,36 @@ const options = {
       web3Contract: new web3.eth.Contract(HegicStaking, "0x840a1AE46B7364855206Eb5b7286Ab7E207e515b")
     },
     {
-      contractName: 'zHEGIC',
-      web3Contract: new web3.eth.Contract(ERC20, "0x837010619aeb2ae24141605afc8f66577f6fb2e7")
-    },
-    {
       contractName: 'LotManager',
       web3Contract: new web3.eth.Contract(LotManager, "0x441ebc9be0bc74657efa1028f87452b3ef6d755e")
     },
     {
       contractName: 'HegicPool',
       web3Contract: new web3.eth.Contract(HegicPool, "0x9e4e091fc8921fe3575eab1c9a6446114f3b5ef2")
+    },
+    {
+      contractName: 'zTreasury',
+      web3Contract: new web3.eth.Contract(zTreasury, "0x3fc929dB2168cFe8E7b5e5370761A4f1b7163911")
+    },
+    {
+      contractName: 'zGovernance',
+      web3Contract: new web3.eth.Contract(zGovernance, "0x7c7b924b4eaed3DA875Bc792b5C1a0b33d118047")
+    },
+    {
+      contractName: 'LP_zLOT-DAI',
+      web3Contract: new web3.eth.Contract(UNIV2, "0x48598b64d88ab649e49e82f9e328eeee5011a8ff")
+    },
+    {
+      contractName: 'LP_HEGIC-zHEGIC',
+      web3Contract: new web3.eth.Contract(UNIV2, "0x6ddc12ef2940137f89af63f05196a4c9d4883ee4")
+    },
+    {
+      contractName: 'LP_zHEGIC-ETH',
+      web3Contract: new web3.eth.Contract(UNIV2, "0x2855d51a6c054e5e879bfce18e3a028ae5c190f7")
+    },
+    {
+      contractName: 'LP_HEGIC-DAI',
+      web3Contract: new web3.eth.Contract(UNIV2, "0x502700f282e6bfc2bb3b805893fadffacf688e7b")
     },
   ],
   events: {
